0cb70bed2236aa48cc403ce2e020191f8bcd3401
Peni code <142409326+PeniTips@users.noreply.github.com>
2023-12-22 12:31:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 "use strict";
 const display = document.querySelector(".output");
+
+//display input value
 function inputValue(input) {
   display.value += input;
 }
+// making the del button functional
 function deleteDisplay() {
-  display.value = String(display.value).slice(0, -1);
+  if (display.value === "error" || !display.value) {
+    display.value = "";
+  } else {
+    display.value = String(display.value).slice(0, -1);
+  }
 }
+
+//making the C button functional
 function clearDisplay() {
   display.value = "";
 }
+//Evaluating values
 function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "error";
+  if (!display.value) {
+    display.value = "";
+  } else {
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = "error";
+    }
   }
 }
